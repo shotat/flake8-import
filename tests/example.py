241cb932b14c1aebedb9d5c4ce7456365e0b303f
shotat <shotat@users.noreply.github.com>
2022-10-12 18:04:50 +0900

    wip

--- tests/example.py
+++ tests/example.py
@@ -11,8 +11,8 @@ from ast import arg
 
 
 def main():
-    print('Hello, World!')  # TODO(momijiame): something
+    print("Hello, World!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
